379244dbaae9662428fc91dc3d4d99a18a0ece25
bluestudyhard <3164752596@qq.com>
2024-03-14 23:44:15 +0800

    feat success connet tiptap collrable

--- src/pages/personal/index.vue
+++ src/pages/personal/index.vue
@@ -6,19 +6,18 @@
 -->
 <script setup lang="ts">
 import StarterKit from '@tiptap/starter-kit'
+
 import { EditorContent, useEditor } from '@tiptap/vue-3'
 import Collaboration from '@tiptap/extension-collaboration'
 import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
 import { TiptapCollabProvider } from '@hocuspocus/provider'
 import * as Y from 'yjs'
 
-// import { WebsocketProvider } from 'y-websocket'
-
 const ydoc = new Y.Doc()
 const provider = new TiptapCollabProvider({
   name: 'blue',
   appId: 'rm8oj29o',
-  token: '1c1a5208b1430a17eb4e1b8beef9340eb3b81edc433e35928d811a017437c698',
+  token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTA0MzA3NDIsIm5iZiI6MTcxMDQzMDc0MiwiZXhwIjoxNzEwNTE3MTQyLCJpc3MiOiJodHRwczovL2Nsb3VkLnRpcHRhcC5kZXYiLCJhdWQiOiJybThvajI5byJ9.QU-ZbTC6YsAK5rOjPMxq9QRwn0UyAH3e7bbUPoLi0mU', // 原来是默认用的jwt，没有jwt就无法连接
   document: ydoc,
 })
 const editor = useEditor({
@@ -41,6 +40,8 @@ const editor = useEditor({
     }),
   ],
 })
+// import { WebsocketProvider } from 'y-websocket'
+
 onMounted(() => {
   provider.connect()
 
@@ -50,7 +51,7 @@ onMounted(() => {
   // 防止刷新后重复添加初始内容
   if (!ydoc.getMap('config').get('initialContentLoaded') && editor) {
     ydoc.getMap('config').set('initialContentLoaded', true)
-    editor.value?.commands.setContent('<p>Initial content</p>')
+    editor.value?.commands.setContent('')
   }
 })
 </script>
